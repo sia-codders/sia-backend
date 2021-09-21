f6574920d1cb97c673a277f2620d21da2a556945
SANTIAGO-PC <SANTIAGO-PC@TOSHIBA>
2021-09-21 03:19:09 +0100

    correccion consulta-baseusuario

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo "NO INGRESE";
+?>
+<img src="http://localhost/sia-backend/imagenes/499.jpg" alt="alt"/>
